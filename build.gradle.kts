93e036fc18aa49b9521e86295d63ec52be340339
André Diogo <redan.blue27@gmail.com>
2018-06-06 22:24:56 +0100

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+subprojects {
+    group = "pt.um.tf.taskmux"
+    version = "1.0-SNAPSHOT"
+
+    apply {
+        plugin("java")
+    }
+
+    extra.set("junit-jupiter_version", "5.2.0")
+    extra.set("junit-platform_version", "1.2.0")
+    extra.set("spread_version", "4.4.0")
+    extra.set("catalyst_version", "1.2.1")
+    extra.set("ekit_version", "1.2-SNAPSHOT")
+
+    repositories {
+        mavenCentral()
+        mavenLocal()
+    }
+
+
+}
+
---
+++ muxer-commons/build.gradle.kts
@@ -0,0 +1,25 @@
+apply {
+    plugin("java-library")
+}
+
+version = "1.0-SNAPSHOT"
+
+
+dependencies {
+    testRuntime("org.junit.platform", "junit-platform-launcher", extra["junit-platform_version"] as String)
+    testRuntime("org.junit.platform", "junit-platform-runner", extra["junit-platform_version"] as String)
+    testRuntime("org.junit.platform", "junit-platform-engine", extra["junit-platform_version"] as String)
+    testRuntime("org.junit.platform", "junit-jupiter-engine", extra["junit-jupiter_version"] as String)
+    testCompile("org.junit.platform", "junit-jupiter-api", extra["junit-jupiter_version"] as String)
+}
+
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_10
+    targetCompatibility = JavaVersion.VERSION_1_10
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform {
+        includeEngines("junit-jupiter")
+    }
+}
