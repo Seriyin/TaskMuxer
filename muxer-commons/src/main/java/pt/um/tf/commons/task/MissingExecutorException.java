c95d25ed01453dfaf4624921a0d1693948c5fd8e
André Diogo <redan.blue27@gmail.com>
2018-06-17 19:08:33 +0100

    Makes exception serializable.

--- muxer-commons/src/main/java/pt/um/tf/commons/task/MissingExecutorException.java
+++ muxer-commons/src/main/java/pt/um/tf/commons/task/MissingExecutorException.java
@@ -1,8 +1,19 @@
 package pt.um.tf.commons.task;
 
-public class MissingExecutorException extends Exception {
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.CatalystSerializable;
+import io.atomix.catalyst.serializer.Serializer;
+
+public class MissingExecutorException extends Exception implements CatalystSerializable {
     @Override
     public String getMessage() {
         return "Missing Executor to execute task in.";
     }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {}
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {}
 }
