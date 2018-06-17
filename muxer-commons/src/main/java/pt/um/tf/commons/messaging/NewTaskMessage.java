25cc23737a391109fab07b22f3443601c48eb73d
André Diogo <redan.blue27@gmail.com>
2018-06-17 19:36:18 +0100

    Type Erases Task. It's up to the client to instantiate it back as correct concrete type.

--- muxer-commons/src/main/java/pt/um/tf/commons/messaging/NewTaskMessage.java
+++ muxer-commons/src/main/java/pt/um/tf/commons/messaging/NewTaskMessage.java
@@ -4,26 +4,32 @@ import io.atomix.catalyst.buffer.BufferInput;
 import io.atomix.catalyst.buffer.BufferOutput;
 import io.atomix.catalyst.serializer.CatalystSerializable;
 import io.atomix.catalyst.serializer.Serializer;
+import io.atomix.catalyst.transport.Address;
 import pt.um.tf.commons.task.Task;
 
-public class NewTaskMessage<T> implements CatalystSerializable {
-    private Task<T> t;
+public class NewTaskMessage implements CatalystSerializable {
+    private Task task;
 
-    public NewTaskMessage(Task<T> t) {
-        this.t = t;
+
+    public NewTaskMessage(Task task, String clientId) {
+        this.task = task;
     }
 
-    public Task<T> getTask() {
-        return t;
+    protected NewTaskMessage() {}
+
+
+
+    public Task getTask() {
+        return task;
     }
 
     @Override
     public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
-        t.writeObject(buffer, serializer);
+        task.writeObject(buffer, serializer);
     }
 
     @Override
     public void readObject(BufferInput<?> buffer, Serializer serializer) {
-        t = serializer.readObject(buffer);
+        task = serializer.readObject(buffer);
     }
 }
