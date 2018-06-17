b11a1ef533f91d27f39977c5a508e9d4d7555627
André Diogo <redan.blue27@gmail.com>
2018-06-17 19:41:21 +0100

    Adds a duplicate exception in case an inbound task is already being executed by client.

---
+++ muxer-server/src/main/java/pt/um/tf/taskmux/server/DuplicateException.java
@@ -0,0 +1,21 @@
+package pt.um.tf.taskmux.server;
+
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.CatalystSerializable;
+import io.atomix.catalyst.serializer.Serializer;
+
+public class DuplicateException extends Exception implements CatalystSerializable {
+    @Override
+    public String getMessage() {
+        return super.getMessage() + System.lineSeparator() + "Duplicate task present, retry get";
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+    }
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {}
+
+}
