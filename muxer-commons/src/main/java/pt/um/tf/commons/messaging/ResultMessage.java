c667382ca28d45ec924a2c6002c67c5919995f8a
André Diogo <redan.blue27@gmail.com>
2018-06-17 19:36:43 +0100

    Adds a type erased result message.

---
+++ muxer-commons/src/main/java/pt/um/tf/commons/messaging/ResultMessage.java
@@ -0,0 +1,33 @@
+package pt.um.tf.commons.messaging;
+
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.CatalystSerializable;
+import io.atomix.catalyst.serializer.Serializer;
+import pt.um.tf.commons.task.Result;
+
+public class ResultMessage implements CatalystSerializable {
+    private Result result;
+
+    public ResultMessage(Result result) {
+        this.result = result;
+    }
+
+    protected ResultMessage() {
+        this.result = result;
+    }
+
+    public Result getResult() {
+        return result;
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+        serializer.writeObject(result);
+    }
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {
+        result = serializer.readObject(buffer);
+    }
+}
