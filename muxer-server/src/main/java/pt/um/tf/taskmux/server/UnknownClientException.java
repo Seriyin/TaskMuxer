b169979c6e33bce4231fba2a94f2defb61d07658
André Diogo <redan.blue27@gmail.com>
2018-06-17 19:41:56 +0100

    Adds an exception when a client is somehow unknown.

---
+++ muxer-server/src/main/java/pt/um/tf/taskmux/server/UnknownClientException.java
@@ -0,0 +1,19 @@
+package pt.um.tf.taskmux.server;
+
+import io.atomix.catalyst.buffer.BufferInput;
+import io.atomix.catalyst.buffer.BufferOutput;
+import io.atomix.catalyst.serializer.CatalystSerializable;
+import io.atomix.catalyst.serializer.Serializer;
+
+public class UnknownClientException extends Exception implements CatalystSerializable {
+    @Override
+    public String getMessage() {
+        return super.getMessage() + System.lineSeparator() + "Unknown Client";
+    }
+
+    @Override
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {}
+
+    @Override
+    public void readObject(BufferInput<?> buffer, Serializer serializer) {}
+}
