f3df40b1969f6e33208c99764ae1202db5df4182
André Diogo <redan.blue27@gmail.com>
2018-06-18 23:47:05 +0100

    Adds simple logging to console output.

--- muxer-client/build.gradle.kts
+++ muxer-client/build.gradle.kts
@@ -7,6 +7,8 @@ version = "1.0-SNAPSHOT"
 
 dependencies {
     implementation("pt.haslab", "ekit", extra["ekit_version"] as String)
+    implementation("org.slf4j", "slf4j-api", extra["slf4j_version"] as String)
+    implementation("org.slf4j", "slf4j-simple", extra["slf4j_version"] as String)
     testRuntime("org.junit.platform", "junit-platform-launcher", extra["junit-platform_version"] as String)
     testRuntime("org.junit.platform", "junit-platform-runner", extra["junit-platform_version"] as String)
     testRuntime("org.junit.platform", "junit-platform-engine", extra["junit-platform_version"] as String)
