e30c216f9476c9c3e7b928458a36dd2f1d242623
André Diogo <redan.blue27@gmail.com>
2018-06-18 23:51:27 +0100

    Refactors common errors into its own commons package.

--- muxer-commons/src/main/java/pt/um/tf/commons/error/DuplicateException.java
+++ muxer-commons/src/main/java/pt/um/tf/commons/error/DuplicateException.java
@@ -1,19 +1,21 @@
-package pt.um.tf.commons.task;
+package pt.um.tf.commons.error;
 
 import io.atomix.catalyst.buffer.BufferInput;
 import io.atomix.catalyst.buffer.BufferOutput;
 import io.atomix.catalyst.serializer.CatalystSerializable;
 import io.atomix.catalyst.serializer.Serializer;
 
-public class MissingExecutorException extends Exception implements CatalystSerializable {
+public class DuplicateException extends Exception implements CatalystSerializable {
     @Override
     public String getMessage() {
-        return "Missing Executor to execute task in.";
+        return super.getMessage() + System.lineSeparator() + "Duplicate task present, retry get";
     }
 
     @Override
-    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {}
+    public void writeObject(BufferOutput<?> buffer, Serializer serializer) {
+    }
 
     @Override
     public void readObject(BufferInput<?> buffer, Serializer serializer) {}
+
 }
